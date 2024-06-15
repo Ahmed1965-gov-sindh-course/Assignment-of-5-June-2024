7f8695b3aabc1683403be01952efe3fab755fb16
Ahmed1965 <ahmedmuhammed786@gmail.com>
2024-06-15 21:44:19 +0500

    pyramid in typescript

---
+++ class-12-6-2024/doWhileLoop.ts
@@ -0,0 +1,5 @@
+do{
+    let n = 10;
+    n++
+    while n>=0
+}
---
+++ class-12-6-2024/waterplant.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+for (let i = 0; i < 5; i++) {
+    console.log(`Watering plant ${i + 1}`);
+}
